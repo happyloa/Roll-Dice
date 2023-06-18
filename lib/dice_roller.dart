510185b640d9e57e688abfe49f06f4c44eef6870
Wolfpack <piafyoyo06@gmail.com>
2023-06-18 21:41:35 +0800

    feat: 新增 dice_roller 檔案，其為 StatefulWidget

---
+++ lib/dice_roller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/Images/dice-6.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/Images/dice-2.png';
+  }
+
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
