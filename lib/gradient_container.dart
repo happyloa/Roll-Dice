ae563f40b73d1d8ba90e69373615fdc48175778e
Wolfpack <piafyoyo06@gmail.com>
2023-06-18 21:42:16 +0800

    refactor: 將擲骰子 Widget 拆分為個別檔案

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice/dice_roller.dart';
+
 // 將漸層方向宣告成常數
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -15,10 +17,6 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice() {
-    // ...
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -29,29 +27,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-3.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
